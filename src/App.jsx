910f519b1a7bf13866410a083f848a5fa1d6cc4d
Connor <connorjohnhughes@gmail.com>
2026-04-13 22:33:38 -0700

    Complete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from './card'
+import Navbar from './navbar'
+
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <Card/>
+        </>
+    )
+}
